9e357e2da1bafbca8de12354a6f010d0f4f21de7
Nathan D'souza <nathan.m.dsouza@gmail.com>
2018-07-31 00:11:34 -0400

    Renamed files, added some functions

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,12 +2,12 @@
 using System.Collections;
 using System.Linq;
 
-namespace ConsoleApp1
+namespace HighCardFlush
 {
-    class HighCardFlush
+    class Game
     {
         int card, money, pLargeF, dLargeF, sTracker;
-        int straightBet, straightProfit, flushBet, flushProfit;
+        int ante, raise, straightBet, straightProfit, flushBet, flushProfit;
         int curStraight, largeStraight;
         int pIndexL, pIndexR, dIndexL, dIndexR;
         bool pQualify, dQualify;
@@ -16,6 +16,11 @@ namespace ConsoleApp1
         int[] dH = new int[7];
         int[] pFCounter = new int[4];
         int[] dFCounter = new int[4];
+
+        private void pay()
+        {
+            money -= (flushBet + straightBet + ante);
+        }
 
         private void deal()
         {
@@ -52,19 +57,23 @@ namespace ConsoleApp1
                 case 4:
                     money += flushBet * 3;
                     flushProfit += flushBet * 2;
+                    raise = ante;
                     break;
 
                 case 5:
                     money += flushBet * 11;
                     flushProfit += flushBet * 10;
+                    raise = ante;
                     break;
                 case 6:
                     money += flushBet * 26;
                     flushProfit += flushBet * 25;
+                    raise = 2 * ante;
                     break;
                 case 7:
                     money += flushBet * 301;
                     flushProfit += flushBet * 300;
+                    raise = 3 * ante;
                     break;
 
                 default:
@@ -121,12 +130,53 @@ namespace ConsoleApp1
         private void playerQualify()
         {
             if (pLargeF < 3) pQualify = false;
-            if (pLargeF > 3) pQualify = true;
-
+            else if (pLargeF > 3) pQualify = true;
+            else
+            {
+                int k = Array.FindIndex(pFCounter, x => x == pLargeF);
+                for (int i = 0; i < k; i++)
+                {
+                    pIndexL += pFCounter[i];                    
+                }
+                pIndexR = pIndexL + pLargeF;
+                pQualify = (pH[pIndexR] / (k + 1) > 8);
+            }
         }
 
+        private void dealerQualify()
+        {
+            if (dLargeF < 3) dQualify = false;
+            else if (dLargeF > 3) dQualify = true;
+            else
+            {
+                int k = Array.FindIndex(dFCounter, x => x == dLargeF);
+                for (int i = 0; i < k; i++)
+                {
+                    dIndexL += dFCounter[i];
+                }
+                dIndexR = dIndexL + dLargeF;
+                dQualify = (dH[dIndexR] / (k + 1) > 6);
+            }
+        }
+
+        private void payRaise()
+        {
+            if (!pQualify) return;
+            else if (!dQualify) money += ante * 2;
+            else if (dLargeF > pLargeF) money -= raise;
+            else if (pLargeF > dLargeF) money += raise + ante * 2;
+            else
+            {
+                for (int i = 0; i < dLargeF; i++)
+                {
+
+                }
+            }
+        }
+        
         public void play()
         {
+            pay();
             deal();
             checkFlush();
             payFlush();
@@ -134,6 +184,8 @@ namespace ConsoleApp1
             payStraight();
             playerQualify();
             dealerQualify();
+            payRaise();
+
         }
     }
 
@@ -142,7 +194,7 @@ namespace ConsoleApp1
        
         static void Main(string[] args)
         {
-            HighCardFlush game = new HighCardFlush;
+            Game game = new Game();
             game.play();
 
         }
